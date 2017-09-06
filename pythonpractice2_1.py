fc51ba84742a1a0a78ceb742db7f86d453be58e7
XtraJos <jasonch97@outlook.co.id>
2017-09-06 22:04:34 +0700

    Practice Lab 2

--- pythonpractice2_1.py
+++ pythonpractice2_1.py
@@ -1,7 +1,7 @@
 x = int(input("Please enter an integer: "))
 y = int(input("Please enter another integer: "))
 if x>y:
-	print(x, "is greater than", y)
+	print(x,"is greater than",y)
 else :
-	print(y, "is greater than or equal to", x)
-	Print("Done")
+	print(y,"is greater than or equal to",x)
+	print("Done")
