edce339a124b6d73bd846ae56af8442f2168d66a
XtraJos <jasonch97@outlook.co.id>
2017-09-07 07:45:31 +0700

    Practice Lab 2

---
+++ pythonpractice2_4.py
@@ -0,0 +1,10 @@
+age = int(input("Please enter your age: "))
+member = True
+if age <= 15:
+	member = False
+if age >= 18:
+	member = False
+if member:
+	print("you can join")
+else:
+	print("you can't join")
